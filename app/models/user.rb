e6784648ba0e9f9b07880f60e4e7f213d412b8a7
kazuki.yukiiri <yukiiri.kazuki@gmail.com>
2020-05-18 02:40:05 +0000

    favorite, unfavorite

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,11 @@ class User < ApplicationRecord
   has_many :followings, through: :relationships, source: :follow
   has_many :reverses_of_relationship, class_name: 'Relationship', foreign_key: 'follow_id'
   has_many :followers, through: :reverses_of_relationship, source: :user
+  #UserモデルからFavorite(中間テーブル)を見た時
+  has_many :favorites
+  #Userモデルが中間テーブル(favorites)を経由してmicropostモデルに繋がっている
+  #micropostsを一度使っているので、favorite_micropostsと別の言い方に変える
+  has_many :favorite_microposts, through: :favorites, source: :micropost
   
   def follow(other_user)
     unless self == other_user
@@ -29,5 +34,22 @@ class User < ApplicationRecord
   
   def feed_microposts
     Micropost.where(user_id: self.following_ids + [self.id])
+  end
+  
+  #Userがお気に入り追加・削除を行うメソッドを定義する
+  #参照しているデータは中間テーブル
+  def favorite(micropost)
+    favorites.find_or_create_by(micropost_id: micropost.id)
+  end
+  
+  def unfavorite(micropost)
+    favorite = favorites.find_by(micropost_id: micropost.id)
+    favorite.destroy if favorite
+  end
+  
+  #既にお気に入りに追加している Micropost かどうかを調べる処理
+  #参照しているデータは中間テーブルを経由したmicropostsテーブル(self.favorite_micropostsにより複数のMicropostを取得)
+  def favorited?(micropost)
+    self.favorite_microposts.include?(micropost)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -11,9 +11,11 @@ Rails.application.routes.draw do
     member do
       get :followings
       get :followers
+      get :favorite_microposts
     end
   end
   
   resources :microposts, only: [:create, :destroy]
   resources :relationships, only: [:create, :destroy]
+  resources :favorites, only: [:create, :destroy]
 end
